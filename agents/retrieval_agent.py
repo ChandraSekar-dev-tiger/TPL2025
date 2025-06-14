267959d02f91f04a6f56f6cb003aef700a2d83f7
abin-et-tiger <abin.emmanuvel@tigeranalytics.com>
2025-06-14 14:18:27 +0530

    update rag agent

--- agents/retrieval_agent.py
+++ agents/retrieval_agent.py
@@ -12,7 +12,30 @@ class RetrievalState(TypedDict):
 
 def retrieval_agent(state: RetrievalState) -> RetrievalState:
     try:
-        filtered_metadata = {"filtered_metadata": "dummy_value"}  # Replace with actual parsing logic
+        #TODO: # Replace with actual parsing logic from azure AI search
+        filtered_metadata = [{
+            "Column Name": "patient_arrival_count_monthly",
+            "Data Type": "int64",
+            "Description": "Total number of patient arrivals in the month",
+            "Granularity": "Monthly",
+            "Calculation": "Sum of patient arrivals for the month"
+            },
+            {
+            "Column Name": "patient_arrival_count_daily",
+            "Data Type": "object",
+            "Description": "Daily count of patient arrivals (JSON format)",
+            "Granularity": "Daily (nested)",
+            "Calculation": "Aggregated from daily patient logs, may include fractional values due to smoothing or averaging"
+            },
+        {
+            "Column Name": "avg_waiting_time_minutes_monthly",
+            "Data Type": "float64",
+            "Description": "Average waiting time (arrival to first treatment) in minutes (monthly)",
+            "Granularity": "Monthly",
+            "Calculation": "Average wait time per month"
+            },
+        ]
+
         state["filtered_metadata"] = filtered_metadata
         logger.info(f"Metadata filtered: {filtered_metadata}")
     except Exception as e:
